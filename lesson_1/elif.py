d7f1b2cb4e0034b5c29e6523cdbab7662fbb9467
Vitalii145 <meizum3s17@gmail.com>
2026-06-30 20:53:55 +0300

    lesson_1

---
+++ lesson_1/elif.py
@@ -0,0 +1,16 @@
+
+
+# age = 15
+# if age >= 18:
+#     print('Adult')
+
+
+# bun = 35
+# money = int(input("How much money do you have?"))
+# if money >= bun:
+#     print('You can buy a loaf of bread')
+# else:
+#     print('What a shame')
+
+dama = 20
+resuld = "adult" if dama >= 18 else "child"
---
+++ lesson_1/python_remember.py
@@ -0,0 +1,18 @@
+raw_string = r"C:\Program Files\Home\Python"
+print(raw_string)
+words = ["Hello", "World" , "and", "Python"]
+result = "_".join(words)
+print(result)
+
+st = 'av' * 7
+print(st)
+
+st = 35*'*'
+print(st)
+
+dy = 'a'
+if dy == 'a' or dy == 'b' or dy == 'c' or dy == 'd':
+    print('YES')
+
+if dy in 'abcd':
+    print('YES')
